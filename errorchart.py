4af8f55d587cd5944682b88813cc48a6eb720ddb
Yunhui <songyunhui2008@gmail.com>
2018-12-24 09:24:27 -0800

    Add more basic samples.

---
+++ errorchart.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 4, 0.2)
+y = np.exp(-x)
+e1 = 0.1 * np.abs(np.random.randn(len(y)))
+e2 = 0.1 * np.abs(np.random.randn(len(y)))
+plt.errorbar(x, y, yerr=[e1,e2], xerr=[e2,e1], fmt='.-', capsize=0)
+plt.show()
