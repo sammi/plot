fb2fc85a1ba66087b507aab26c6fc02a45afb2d6
Yunhui <songyunhui2008@gmail.com>
2018-12-25 16:49:27 -0800

    Update animation samples.

--- animation/line.py
+++ animation/line.py
@@ -8,27 +8,20 @@ x = np.arange(0, 2*np.pi, 0.01)
 line, = ax.plot(x, np.sin(x))
 
 
-def init():  # only required for blitting to give a clean slate.
+def init():
     line.set_ydata([np.nan] * len(x))
     return line,
 
 
 def animate(i):
-    line.set_ydata(np.sin(x + i / 100))  # update the data.
+    line.set_ydata(np.sin(x + i / 100))
     return line,
 
 
-ani = animation.FuncAnimation(
-    fig, animate, init_func=init, interval=2, blit=True, save_count=50)
+ani = animation.FuncAnimation(fig, animate, init_func=init, interval=2, blit=True, save_count=50)
 
-# To save the animation, use e.g.
-#
-# ani.save("movie.mp4")
-#
-# or
-#
-# from matplotlib.animation import FFMpegWriter
-# writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-# ani.save("movie.mp4", writer=writer)
+from matplotlib.animation import FFMpegWriter
+writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+ani.save("movie.mp4", writer=writer)
 
 plt.show()
