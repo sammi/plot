4c9bb6524a756ffb57016d7aea1d040777925942
Sammi Song <songyunhui2008@gmail.com>
2018-12-30 12:42:17 -0800

    Add python lint dependencies.

--- radar/multiple.py
+++ radar/multiple.py
@@ -3,15 +3,15 @@ import pandas as pd
 from math import pi
 
 df = pd.DataFrame({
-'group': ['A','B','C','D'],
-'f1': [38, 1.5, 30, 4],
-'f2': [29, 10, 9, 34],
-'f3': [8, 39, 23, 24],
-'f4': [7, 31, 33, 14],
-'f5': [28, 15, 32, 14]
+    'group': ['A', 'B', 'C', 'D'],
+    'f1': [38, 1.5, 30, 4],
+    'f2': [29, 10, 9, 34],
+    'f3': [8, 39, 23, 24],
+    'f4': [7, 31, 33, 14],
+    'f5': [28, 15, 32, 14]
 })
 
-categories=list(df)[1:]
+categories = list(df)[1:]
 N = len(categories)
 
 angles = [n / float(N) * 2 * pi for n in range(N)]
@@ -25,16 +25,16 @@ ax.set_theta_direction(-1)
 plt.xticks(angles[:-1], categories)
 
 ax.set_rlabel_position(0)
-plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
-plt.ylim(0,40)
+plt.yticks([10, 20, 30], ["10", "20", "30"], color="grey", size=7)
+plt.ylim(0, 40)
 
 
-values=df.loc[0].drop('group').values.flatten().tolist()
+values = df.loc[0].drop('group').values.flatten().tolist()
 values += values[:1]
 ax.plot(angles, values, linewidth=1, linestyle='solid', label="group A")
 ax.fill(angles, values, 'b', alpha=0.1)
 
-values=df.loc[1].drop('group').values.flatten().tolist()
+values = df.loc[1].drop('group').values.flatten().tolist()
 values += values[:1]
 ax.plot(angles, values, linewidth=1, linestyle='solid', label="group B")
 ax.fill(angles, values, 'r', alpha=0.1)
--- radar/single.py
+++ radar/single.py
@@ -3,7 +3,7 @@ import pandas as pd
 from math import pi
 
 df = pd.DataFrame({
-    'group': ['A','B','C','D'],
+    'group': ['A', 'B', 'C', 'D'],
     'f1': [38, 1.5, 30, 4],
     'f2': [29, 10, 9, 34],
     'f3': [8, 39, 23, 24],
@@ -11,26 +11,26 @@ df = pd.DataFrame({
     'f5': [28, 15, 32, 14]
 })
 
-categories=list(df)[1:]
+categories = list(df)[1:]
 N = len(categories)
- 
-values=df.loc[0].drop('group').values.flatten().tolist()
+
+values = df.loc[0].drop('group').values.flatten().tolist()
 values += values[:1]
 values
- 
+
 angles = [n / float(N) * 2 * pi for n in range(N)]
 angles += angles[:1]
- 
+
 ax = plt.subplot(111, polar=True)
- 
+
 plt.xticks(angles[:-1], categories, color='grey', size=8)
- 
+
 ax.set_rlabel_position(0)
-plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
-plt.ylim(0,40)
- 
+plt.yticks([10, 20, 30], ["10", "20", "30"], color="grey", size=7)
+plt.ylim(0, 40)
+
 ax.plot(angles, values, linewidth=1, linestyle='solid')
- 
+
 ax.fill(angles, values, 'b', alpha=0.1)
 
 plt.show()
